c36c590153d710752f2de1bb5503437a0256fd97
Adrian <marcelonads@gmail.com>
2024-01-19 15:43:00 +0800

    refactor: ADDED new property called very_nearby
    This new property will indicate if the merchant is within the minimum
    kilometers of the system (currently 2 kms) in order for the driver to directly charge to
    it.

--- repository/MerchantsRepository.js
+++ repository/MerchantsRepository.js
@@ -33,7 +33,8 @@ module.exports = class MerchantsRepository {
                   POWER(SIN((${lat} - ABS(address_lat)) * PI() / 180 / 2), 2) +
                   COS(${lat} * PI() / 180) * COS(ABS(address_lat) * PI() / 180) *
                   POWER(SIN((${lng} - address_lng) * PI() / 180 / 2), 2)
-                )), 1) AS distance
+                )), 1) AS distance,
+				(SELECT CASE WHEN distance <= 2 THEN 1 ELSE 0 END) AS very_nearby
               FROM
                 user_merchants
               HAVING
@@ -75,7 +76,8 @@ module.exports = class MerchantsRepository {
 					  POWER(SIN((${location.lat} - ABS(address_lat)) * PI() / 180 / 2), 2) +
 					  COS(${location.lat} * PI() / 180) * COS(ABS(address_lat) * PI() / 180) *
 					  POWER(SIN((${location.lng} - address_lng) * PI() / 180 / 2), 2)
-					)), 1) AS distance
+					)), 1) AS distance,
+					(SELECT CASE WHEN distance <= 2 THEN 1 ELSE 0 END) AS very_nearby
 					FROM
 					user_merchants
 					INNER JOIN ev_chargers
@@ -196,7 +198,8 @@ module.exports = class MerchantsRepository {
 			  POWER(SIN((? - ABS(address_lat)) * PI() / 180 / 2), 2) +
 			  COS(? * PI() / 180) * COS(ABS(address_lat) * PI() / 180) *
 			  POWER(SIN((? - address_lng) * PI() / 180 / 2), 2)
-			)), 1) AS distance
+			)), 1) AS distance,
+			(SELECT CASE WHEN distance <= 2 THEN 1 ELSE 0 END) AS very_nearby
 			FROM
 			user_merchants
 			LEFT JOIN favorite_merchants
@@ -231,7 +234,8 @@ module.exports = class MerchantsRepository {
 					  POWER(SIN((? - ABS(address_lat)) * PI() / 180 / 2), 2) +
 					  COS(? * PI() / 180) * COS(ABS(address_lat) * PI() / 180) *
 					  POWER(SIN((? - address_lng) * PI() / 180 / 2), 2)
-					)), 1) AS distance
+					)), 1) AS distance,
+					(SELECT CASE WHEN distance <= 2 THEN 1 ELSE 0 END) AS very_nearby
 					FROM
 					user_merchants
 					LEFT JOIN favorite_merchants
